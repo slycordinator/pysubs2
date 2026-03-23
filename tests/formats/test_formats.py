4aa02ec12d3491803d31efb8ea118a471fa89aaa
Christopher Slycord <cslycord+1@gmail.com>
2026-03-23 12:36:18 +0900

    change test_formats.py to import used extensions/functions
    Instead of importing pysubs2, download the used used functions and/or classes.
    
    * Import exception classes called directly from pysubs.exceptions
    * Import used format functions from pysubs2.formats
    * Import the SSAFile class from pysubsbs.ssafile

--- tests/formats/test_formats.py
+++ tests/formats/test_formats.py
@@ -1,30 +1,32 @@
 import multiprocessing
 import itertools
 import pytest
-import pysubs2
 
+from pysubs2.exceptions import UnknownFormatIdentifierError, UnknownFileExtensionError, FormatAutodetectionError
+from pysubs2.formats import get_format_class, get_format_identifier, get_file_extension, autodetect_format
+from pysubs2.ssafile import SSAFile
 
 def test_identifier_to_class() -> None:
-    with pytest.raises(pysubs2.exceptions.UnknownFormatIdentifierError) as exc_info:
-        pysubs2.formats.get_format_class("unknown-format-identifier")
+    with pytest.raises(UnknownFormatIdentifierError) as exc_info:
+        get_format_class("unknown-format-identifier")
     assert exc_info.value.format_ == "unknown-format-identifier"
 
 
 def test_ext_to_identifier() -> None:
-    with pytest.raises(pysubs2.exceptions.UnknownFileExtensionError) as exc_info:
-        pysubs2.formats.get_format_identifier(".xyz")
+    with pytest.raises(UnknownFileExtensionError) as exc_info:
+        get_format_identifier(".xyz")
     assert exc_info.value.ext == ".xyz"
 
 
 def test_identifier_to_ext() -> None:
-    with pytest.raises(pysubs2.exceptions.UnknownFormatIdentifierError) as exc_info:
-        pysubs2.formats.get_file_extension("unknown-format-identifier")
+    with pytest.raises(UnknownFormatIdentifierError) as exc_info:
+        get_file_extension("unknown-format-identifier")
     assert exc_info.value.format_ == "unknown-format-identifier"
 
 
 def test_format_detection_fail() -> None:
-    with pytest.raises(pysubs2.exceptions.FormatAutodetectionError) as exc_info:
-        pysubs2.formats.autodetect_format("")
+    with pytest.raises(FormatAutodetectionError) as exc_info:
+        autodetect_format("")
     assert not exc_info.value.formats
 
 
@@ -33,7 +35,7 @@ def _test_97_func(i: int, error: int) -> int:
         if error == 0:
             raise RuntimeError("Regular exception is properly handled by multiprocessing")
         elif error == 1:
-            pysubs2.SSAFile.from_string("")
+            SSAFile.from_string("")
         else:
             assert False
     else:
@@ -46,5 +48,5 @@ def test_97_format_detection_multiprocessing_error() -> None:
     with multiprocessing.Pool(4) as pool:
         with pytest.raises(RuntimeError):
             pool.starmap(_test_97_func, zip(range(4), itertools.repeat(0)))
-        with pytest.raises(pysubs2.exceptions.FormatAutodetectionError):
+        with pytest.raises(FormatAutodetectionError):
             pool.starmap(_test_97_func, zip(range(4), itertools.repeat(1)))
