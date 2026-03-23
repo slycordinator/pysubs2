934d507bf51638a4794717b14c209b6a60ec94d5
Christopher Slycord <cslycord+1@gmail.com>
2026-03-23 10:35:04 +0900

    update test_formats.py for correct exceptions
    Change to raise "pysubs2.exceptions.xError" instead of "pysubs2.xError"

--- tests/formats/test_formats.py
+++ tests/formats/test_formats.py
@@ -5,25 +5,25 @@ import pysubs2
 
 
 def test_identifier_to_class() -> None:
-    with pytest.raises(pysubs2.UnknownFormatIdentifierError) as exc_info:
+    with pytest.raises(pysubs2.exceptions.UnknownFormatIdentifierError) as exc_info:
         pysubs2.formats.get_format_class("unknown-format-identifier")
     assert exc_info.value.format_ == "unknown-format-identifier"
 
 
 def test_ext_to_identifier() -> None:
-    with pytest.raises(pysubs2.UnknownFileExtensionError) as exc_info:
+    with pytest.raises(pysubs2.exceptions.UnknownFileExtensionError) as exc_info:
         pysubs2.formats.get_format_identifier(".xyz")
     assert exc_info.value.ext == ".xyz"
 
 
 def test_identifier_to_ext() -> None:
-    with pytest.raises(pysubs2.UnknownFormatIdentifierError) as exc_info:
+    with pytest.raises(pysubs2.exceptions.UnknownFormatIdentifierError) as exc_info:
         pysubs2.formats.get_file_extension("unknown-format-identifier")
     assert exc_info.value.format_ == "unknown-format-identifier"
 
 
 def test_format_detection_fail() -> None:
-    with pytest.raises(pysubs2.FormatAutodetectionError) as exc_info:
+    with pytest.raises(pysubs2.exceptions.FormatAutodetectionError) as exc_info:
         pysubs2.formats.autodetect_format("")
     assert not exc_info.value.formats
 
@@ -46,5 +46,5 @@ def test_97_format_detection_multiprocessing_error() -> None:
     with multiprocessing.Pool(4) as pool:
         with pytest.raises(RuntimeError):
             pool.starmap(_test_97_func, zip(range(4), itertools.repeat(0)))
-        with pytest.raises(pysubs2.FormatAutodetectionError):
+        with pytest.raises(pysubs2.exceptions.FormatAutodetectionError):
             pool.starmap(_test_97_func, zip(range(4), itertools.repeat(1)))
